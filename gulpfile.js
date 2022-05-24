851d85b841eb73ad59ed3f00bd381833517aa6b3
gaowenbin <lotosv2010@163.com>
2022-05-24 22:19:35 +0800

    feat(gulp): 页面模板编译

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,48 @@
-const { src, dest } = require('gulp');
+const { src, dest, series, parallel } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const babel = require('gulp-babel');
+const swig = require('gulp-swig');
+
+const data = {
+  menus: [
+    {
+      name: 'Home',
+      icon: 'aperture',
+      link: 'index.html'
+    },
+    {
+      name: 'Features',
+      link: 'features.html'
+    },
+    {
+      name: 'About',
+      link: 'about.html'
+    },
+    {
+      name: 'Contact',
+      link: '#',
+      children: [
+        {
+          name: 'Twitter',
+          link: 'https://twitter.com/w_zce'
+        },
+        {
+          name: 'About',
+          link: 'https://weibo.com/zceme'
+        },
+        {
+          name: 'divider'
+        },
+        {
+          name: 'About',
+          link: 'https://github.com/zce'
+        }
+      ]
+    }
+  ],
+  pkg: require('./package.json'),
+  date: new Date()
+}
 
 const style = () => {
   return src('src/assets/styles/*.scss', { base: 'src' })
@@ -14,7 +56,14 @@ const script = () => {
     .pipe(dest('dist'));
 }
 
+const page = () => {
+  return src('src/*.html', { base: 'src' })
+    .pipe(swig({ data, defaults: { cache: false } })) // 防止模板缓存导致页面不能及时更新
+    .pipe(dest('dist'));
+}
+
+const compile = parallel(style, script, page);
+
 module.exports = {
-  style,
-  script
+  compile
 }
