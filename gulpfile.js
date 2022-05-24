c1e7864a08dae32b798c4cc6505735b59b8b7ad7
gaowenbin <lotosv2010@163.com>
2022-05-24 22:05:42 +0800

    feat(gulp): 脚本编译

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,20 @@
 const { src, dest } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const babel = require('gulp-babel');
 
 const style = () => {
-  return src('src/assets/styles/*.scss', {
-    base: 'src'
-  })
-  .pipe(sass({
-    outputStyle: 'expanded'
-  }))
+  return src('src/assets/styles/*.scss', { base: 'src' })
+  .pipe(sass({ outputStyle: 'expanded' }))
   .pipe(dest('dist'));
 }
 
+const script = () => {
+  return src('src/assets/scripts/*.js', { base: 'src' })
+    .pipe(babel({ presets: ['@babel/preset-env'] }))
+    .pipe(dest('dist'));
+}
+
 module.exports = {
-  style
+  style,
+  script
 }
